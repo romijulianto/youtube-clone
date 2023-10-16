7bfcde2af7d62e85cfb1c69057af4fa62634d429
Romi Julianto <romyjulians@gmail.com>
2023-10-16 08:15:24 +0700

    feat(#1): add header (2%)

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,18 +1,29 @@
-import React, { useContext, useState } from 'react'
-import Loader from '../common/loader';
+import React, { useContext, useState } from "react";
+import Loader from "../common/loader";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const [searchQuery, setSearchQuery] = useState("")
+  const [searchQuery, setSearchQuery] = useState("");
 
   const { loading, mobileMenu, setMobileMenu } = useContext(Context);
-  return (
-    <div className='sticky top-0 z-10 flex flex-row items-center justify-between px-4 bg-white h-14 md:px-5 dark:bg-black'>
-      {loading && <Loader />}
-        <div className='flex items-center h-5 text-black'>
-            
-        </div>
-    </div>
-  )
-}
 
-export default Header
+  const navigate = useNavigate();
+
+  const searchQueryHandler = (event) => {
+    if (
+      (event?.key === "Enter" || event === "searchButton") &&
+      searchQuery?.length > 0
+    ) {
+      navigate(`/searchResult/${searchQuery}`);
+    }
+  };
+
+  return (
+    <div className="sticky top-0 z-10 flex flex-row items-center justify-between px-4 bg-white h-14 md:px-5 dark:bg-black">
+      {loading && <Loader />}
+      <div className="flex items-center h-5 text-black"></div>
+    </div>
+  );
+};
+
+export default Header;
